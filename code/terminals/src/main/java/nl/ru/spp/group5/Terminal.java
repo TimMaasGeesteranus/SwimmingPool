7eb38605d1e9bf5d21c771d9a34844a29230ba17
timmaasgeesteranus <tim__mg@hotmail.com>
2024-05-20 11:50:22 +0200

    select applet on card

--- code/terminals/src/main/java/nl/ru/spp/group5/Terminal.java
+++ code/terminals/src/main/java/nl/ru/spp/group5/Terminal.java
@@ -30,7 +30,9 @@ public abstract class Terminal{
             try {
                 if (terminal.isCardPresent()) {
                     CardChannel channel = terminal.connect("*").getBasicChannel();
-                    handleCard(channel);
+                    if (appletSelectedSuccessfully(channel)){
+                        handleCard(channel);
+                    }
                     // Wait for card to be removed before checking again
                     while (terminal.isCardPresent()) {
                         Thread.sleep(1000);
@@ -44,4 +46,25 @@ public abstract class Terminal{
     }
 
     abstract public void handleCard(CardChannel channel) throws CardException;
+
+    private boolean appletSelectedSuccessfully(CardChannel channel) throws CardException{
+        byte[] apduBytes = {
+            (byte) 0x00,       // CLA (Class)
+            (byte) 0xA4,       // INS (Instruction - SELECT)
+            (byte) 0x04,       // P1  (Parameter 1 - SELECT by DF Name)
+            (byte) 0x00,       // P2  (Parameter 2 - First or only occurrence)
+            (byte) 0x06,       // Lc  (Length of data)
+            (byte) 0x11,       // AID length (11 bytes for your AID)
+            (byte) 0x22, (byte) 0x33, (byte) 0x44, (byte) 0x55, (byte) 0x66, // AID bytes
+            (byte) 0x00        // Le  (Maximum response length)
+        };
+        CommandAPDU apdu = new CommandAPDU(apduBytes);
+        ResponseAPDU response = channel.transmit(apdu);
+
+        if (response.getSW() != 0x9000){
+            System.out.println("Could not select the applet on the card. Please try again.");
+            return false;
+        }
+        return true;
+    }
 }
