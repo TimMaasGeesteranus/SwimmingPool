1d7d4e124b44444f78c12e62939e50cba6e6fda2
Amit Isvoranu <amit.isvoranu@sue.nl>
2024-05-28 14:40:41 +0200

    logging

--- code/terminals/src/main/java/nl/ru/spp/group5/AccessGateTerminal.java
+++ code/terminals/src/main/java/nl/ru/spp/group5/AccessGateTerminal.java
@@ -1,32 +1,49 @@
 package nl.ru.spp.group5;
 
-import java.util.Scanner;
+import java.io.IOException;
+import java.util.logging.*;
 import javax.smartcardio.*;
 
-public class AccessGateTerminal extends Terminal{
-    public static void main(String[] args){
-        System.out.println("This is the access gate terminal");
+public class AccessGateTerminal extends Terminal {
+    private static final Logger logger = Logger.getLogger(AccessGateTerminal.class.getName());
+
+    static {
+        try {
+            LogManager.getLogManager().reset();
+            FileHandler fileHandler = new FileHandler("access_gate_terminal.log", true);
+            fileHandler.setFormatter(new SimpleFormatter());
+            logger.addHandler(fileHandler);
+            logger.setLevel(Level.ALL);
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Failed to initialize logger handler.", e);
+        }
+    }
+
+    public static void main(String[] args) {
+        logger.info("Starting AccessGateTerminal...");
         AccessGateTerminal accessGateTerminal = new AccessGateTerminal();
         accessGateTerminal.waitForCard();
     }
 
-    public AccessGateTerminal(){
-
+    public AccessGateTerminal() {
+        logger.info("AccessGateTerminal initialized.");
     }
 
     @Override
-    public void handleCard(CardChannel channel) throws CardException{
-        // TODO: Mutual authentication
+    public void handleCard(CardChannel channel) throws CardException {
+        logger.info("Card detected, handling card...");
 
+        // TODO: Mutual authentication
         CommandAPDU apdu = new CommandAPDU((byte) 0x02, (byte) 0x03, (byte) 0x00, (byte) 0x00, (byte) 0x00); // TODO: what bytes are sent when?
         ResponseAPDU response = channel.transmit(apdu);
 
         byte[] responseBytes = response.getData();
         String responseString = new String(responseBytes);
-    
-        switch (responseString) { // TODO: set meaning for bytes
+        logger.info("Received response from card: " + responseString);
+
+        switch (responseString) {
             case "true":
-                openGate();       
+                openGate();
                 break;
             case "false":
                 denyAccess();
@@ -37,16 +54,14 @@ public class AccessGateTerminal extends Terminal{
         }
     }
 
-    public void openGate(){
+    public void openGate() {
+        logger.info("Access granted, opening the gate...");
         System.out.println("Welcome to the swimming pool!");
         System.out.println("...Opening the gate...");
-
-        // TODO: add logging
     }
 
-    public void denyAccess(){
+    public void denyAccess() {
+        logger.warning("Access denied.");
         System.out.println("Access denied...");
-
-        // TODO: add logging
     }
 }
