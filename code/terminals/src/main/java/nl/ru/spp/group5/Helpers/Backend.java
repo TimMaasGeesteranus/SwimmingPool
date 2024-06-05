3296a478186a0fe997afaebe3d6762d4ac80a2ef
Borys Rzepa <borys.rzepa@sue.nl>
2024-06-05 13:52:38 +0200

    ??ddd?

--- code/terminals/src/main/java/nl/ru/spp/group5/Helpers/Backend.java
+++ code/terminals/src/main/java/nl/ru/spp/group5/Helpers/Backend.java
@@ -1,13 +1,28 @@
 package nl.ru.spp.group5.Helpers;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class Backend {
+    private static final String EMPLOYEE_CODE = "123456";
+    private static Set<String> blockedCards = new HashSet<>();
     private static Map<String, Integer> cardEntries = new HashMap<>();
     private static Map<String, Boolean> cardValidity = new HashMap<>();
     private static Map<String, String> cardExpiryDates = new HashMap<>();
-    private static final String employeeCode = "123456";
+
+    public static String getEmployeeCode() {
+        return EMPLOYEE_CODE;
+    }
+
+    public static void blockCard(String cardID) {
+        blockedCards.add(cardID);
+    }
+
+    public static boolean isCardBlocked(String cardID) {
+        return blockedCards.contains(cardID);
+    }
 
     public static void setCardEntries(String cardId, int entries) {
         cardEntries.put(cardId, entries);
@@ -25,19 +40,11 @@ public class Backend {
         return cardValidity.getOrDefault(cardId, false);
     }
 
-    public static void blockCard(String cardId) {
-        cardValidity.put(cardId, false);
-    }
-
-    public static String getEmployeeCode() {
-        return employeeCode;
-    }
-
     public static void setCardExpiryDate(String cardId, String expiryDate) {
         cardExpiryDates.put(cardId, expiryDate);
     }
 
     public static String getCardExpiryDate(String cardId) {
-        return cardExpiryDates.get(cardId);
+        return cardExpiryDates.getOrDefault(cardId, "No expiry date set");
     }
 }
