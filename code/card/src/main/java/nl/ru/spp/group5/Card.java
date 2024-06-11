71dd04ae3c62b4ed4415d907159b46000ba8dff7
timmaasgeesteranus <tim__mg@hotmail.com>
2024-06-11 19:35:15 +0200

    more deletion of unused code

--- code/card/src/main/java/nl/ru/spp/group5/Card.java
+++ code/card/src/main/java/nl/ru/spp/group5/Card.java
@@ -121,27 +121,6 @@ public class Card extends Applet {
 
         byte ins = buffer[ISO7816.OFFSET_INS];
         switch (ins) {
-            case INS_GET_DATA:
-                getData(apdu);
-                break;
-            case INS_SET_DATA:
-                setData(apdu);
-                break;
-            case INS_ISSUE_SEASON_TICKET:
-                issueSeasonTicket(apdu);
-                break;
-            case INS_ISSUE_ENTRY_TICKET:
-                issueEntryTicket();
-                break;
-            case INS_USE_ENTRY:
-                useEntry();
-                break;
-            case INS_CHECK_VALIDITY:
-                checkValidity(apdu);
-                break;
-            case INS_GET_REMAINING_ENTRIES:
-                getRemainingEntries(apdu);
-                break;
             case INS_BLOCK_CARD:
                 blockCard();
                 break;
@@ -168,12 +147,6 @@ public class Card extends Applet {
                 break;
             case INS_GET_ENTRIES:
                 buyTicket.getEntries(apdu);
-                break;
-            case INS_ISSUE_CARD:
-                issueCard(apdu);
-                break;
-            case INS_SAVE_CERTIFICATE:
-                saveCertificate(apdu);
                 break;
             case INS_ISSUE_GENERATEKEYS:
                 init.generateKeys(apdu);
@@ -219,128 +192,10 @@ public class Card extends Applet {
         }
     }
 
-    private void getData(APDU apdu) {
-        byte[] buffer = apdu.getBuffer();
-        short length = (short) data.length;
-        Util.arrayCopy(data, (short) 0, buffer, ISO7816.OFFSET_CDATA, length);
-        apdu.setOutgoingAndSend(ISO7816.OFFSET_CDATA, length);
-    }
-
-    private void setData(APDU apdu) {
-        byte[] buffer = apdu.getBuffer();
-        short length = apdu.setIncomingAndReceive();
-        Util.arrayCopy(buffer, ISO7816.OFFSET_CDATA, data, (short) 0, length);
-    }
-
-    private void issueSeasonTicket(APDU apdu) {
-        byte[] buffer = apdu.getBuffer();
-        short length = apdu.setIncomingAndReceive();
-        if (length != 2) {
-            ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
-        }
-        short duration = Util.getShort(buffer, ISO7816.OFFSET_CDATA);
-        setCurrentDate();
-        addDaysToExpiration(duration);
-    }
-
-    private void checkValidity(APDU apdu) {
-        byte[] buffer = apdu.getBuffer();
-        short currentYear = Util.getShort(buffer, ISO7816.OFFSET_CDATA);
-        byte currentMonth = buffer[ISO7816.OFFSET_CDATA + 2];
-        byte currentDay = buffer[ISO7816.OFFSET_CDATA + 3];
-
-        boolean valid = false;
-
-        if (currentYear < expirationYear) {
-            valid = true;
-        } else if (currentYear == expirationYear) {
-            if (currentMonth < expirationMonth) {
-                valid = true;
-            } else if (currentMonth == expirationMonth && currentDay <= expirationDay) {
-                valid = true;
-            }
-        }
-
-        buffer[0] = (byte) (valid ? 1 : 0);
-        apdu.setOutgoingAndSend((short) 0, (short) 1);
-    }
-
-    private void issueEntryTicket() {
-        entryCounter = 10;
-    }
-
-    private void useEntry() {
-        if (entryCounter > 0) {
-            entryCounter--;
-        } else {
-            ISOException.throwIt((short) 0x6300);
-        }
-    }
-
-    private void getRemainingEntries(APDU apdu) {
-        byte[] buffer = apdu.getBuffer();
-        buffer[0] = entryCounter;
-        apdu.setOutgoingAndSend((short) 0, (short) 1);
-    }
-
     private void blockCard() {
         if (cardState == STATE_ISSUED) {
             cardState = STATE_BLOCKED;
             isBlocked = true;
         }
-    }
-
-    private void issueCard(APDU apdu) {
-        if (cardState == STATE_INITIAL) {
-            byte[] buffer = apdu.getBuffer();
-            short length = apdu.setIncomingAndReceive();
-            Util.arrayCopy(buffer, (short) (ISO7816.OFFSET_CDATA), cardKey, (short) 0, (short) 16);
-            Util.arrayCopy(buffer, (short) (ISO7816.OFFSET_CDATA + 16), kCard, (short) 0, (short) 16);
-            cardState = STATE_ISSUED;
-            isIssued = true;
-        }
-    }
-
-    private void saveCertificate(APDU apdu) {
-        byte[] buffer = apdu.getBuffer();
-        short length = apdu.setIncomingAndReceive();
-        Util.arrayCopy(buffer, ISO7816.OFFSET_CDATA, seasonTicketCertificate, (short) 0, length);
-    }
-
-    private void setCurrentDate() {
-        this.expirationYear = 2024;
-        this.expirationMonth = 11;
-        this.expirationDay = 30;
-    }
-
-    private void addDaysToExpiration(short days) {
-        final short[] daysInMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-        while (days > 0) {
-            short daysInCurrentMonth = daysInMonth[(short) (expirationMonth - 1)];
-            if (expirationMonth == 2 && isLeapYear(expirationYear)) {
-                daysInCurrentMonth++; // Adjust for leap year
-            }
-
-            if ((short) (expirationDay + days) <= daysInCurrentMonth) {
-                expirationDay += days;
-                days = 0;
-            } else {
-                days -= (short) (daysInCurrentMonth - expirationDay + 1);
-                expirationDay = 1;
-                expirationMonth++;
-
-                if (expirationMonth > 12) {
-                    expirationMonth = 1;
-                    expirationYear++;
-                }
-            }
-        }
-    }
-
-    private boolean isLeapYear(short year) {
-        if (year % 4 != 0) return false;
-        if (year % 100 == 0 && year % 400 != 0) return false;
-        return true;
     }
 }
--- code/terminals/src/main/java/nl/ru/spp/group5/Helpers/SecurityProtocols.java
+++ code/terminals/src/main/java/nl/ru/spp/group5/Helpers/SecurityProtocols.java
@@ -247,41 +247,4 @@ public class SecurityProtocols {
             throw new CardException("something went wrong when sending second half x2");
         }
     }
-
-
-    // Method to derive a session key from the card's symmetric key and a nonce
-    public byte[] deriveSessionKey(byte[] nonce) {
-        // Implement logic to derive session key Ksession from Kcard and nonce
-        // Placeholder for session key derivation logic
-        return new byte[]{/* derived session key bytes */};
-    }
-
-    // Method to generate a Message Authentication Code (MAC) for message integrity and authenticity
-    public byte[] generateMAC(byte[] message, byte[] sessionKey) throws Exception {
-        // Implement MAC generation using sessionKey
-        Mac mac = Mac.getInstance("HmacSHA256");
-        SecretKeySpec keySpec = new SecretKeySpec(sessionKey, "HmacSHA256");
-        mac.init(keySpec);
-        return mac.doFinal(message);
-    }
-
-    // Method to verify the certificate of a season ticket using the public key of the vending machine
-    public boolean verifyCertificate(byte[] cert, byte[] publicKey) {
-        // Placeholder for certificate verification logic
-        return true; // Assume the certificate is valid for demonstration
-    }
-
-    // Method to encrypt data using the given key
-    public byte[] encryptData(byte[] data, Key key) {
-        // Placeholder for encryption logic
-        return data; // Return unmodified data for demonstration
-    }
-
-    // Method to decrypt data using the given key
-    public byte[] decryptData(byte[] data, Key key) {
-        // Placeholder for decryption logic
-        return data; // Return unmodified data for demonstration
-    }
-
-
 }
